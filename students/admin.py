ef131c745cfe2a430e8f748f233421eebf83dd10
gauravdaidonho <gauravgurungu1@gmail.com>
2026-08-30 23:45:05 +0545

    Fix merge conflicts in student files

--- students/admin.py
+++ students/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+
 from .models import Student
 
+
 @admin.register(Student)
-class Studentadmin(admin.ModelAdmin) : 
+class Studentadmin(admin.ModelAdmin):
     list_display = ("name", "email", "phone", "course")
     list_filter = ("course",)
-    search_fields = ("name" , "email")
->>>>>>> 1dc98b9 (Save my work)
+    search_fields = ("name", "email")
--- students/models.py
+++ students/models.py
@@ -3,15 +3,18 @@ from academics.models import Course
 
 
 class Student(models.Model):
+
     name = models.CharField(max_length=100)
+
     email = models.EmailField(unique=True)
+
     phone = models.CharField(max_length=15)
+
     course = models.ForeignKey(
         Course,
         on_delete=models.PROTECT,
         related_name="students",
     )
->>>>>>> 1dc98b9 (Save my work)
 
     def __str__(self):
         return self.name
