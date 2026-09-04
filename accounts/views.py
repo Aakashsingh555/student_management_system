14099e9fc0422f4d68bec10eef6b58de3894988c
gauravdaidonho <gauravgurungu1@gmail.com>
2026-09-04 19:20:10 +0545

    Make dashboard student and course stats dynamic

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from students.models import Student
+from academics.models import Course
 
 
 def dashboard(request):
     students = Student.objects.all()
+    total_students = Student.objects.count()
+    total_course = Course.objects.count()
 
     return render(request, 'dashboard.html', {
-        'students': students
+        'students': students,
+        'total_students': total_students,
+        'total_course' : total_course,
     })
